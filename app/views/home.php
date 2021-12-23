0bd2207005183c0f125420a5929e5abb2548408f
kesaralive <kesaralive@gmail.com>
2021-12-23 19:56:30 +0530

    create public pages

--- app/views/home.php
+++ app/views/home.php
@@ -5,10 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo SITENAME; ?></title>
+    <link rel="stylesheet" href="<?php echo URLROOT . '/public/css/main.css' ?>">
 </head>
 
 <body>
-    <h1>API HOME</h1>
+    <nav>
+        <div class="logo">Flexguru</div>
+        <div class="menu">
+            <a href="#">Visit Website</a><a href="#">Contact us</a><a href="#">Help</a>
+        </div>
+    </nav>
+    <main>
+        <div class="cover-image">
+            <img src="" alt="Flexguru API Services">
+        </div>
+        <div class="site-heading">
+            <h1>Flexguru API Services</h1>
+        </div>
+    </main>
 </body>
 
 </html>
