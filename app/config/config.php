98d53c1505f32a9904ad7f85d2622b552609082a
kesaralive <kesaralive@gmail.com>
2021-12-23 11:20:21 +0530

    fix: reset .gitignore

---
+++ app/config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+//Database params
+define('DB_HOST', 'localhost'); //Add your db host
+define('DB_USER', 'flexadmin'); // Add your DB root
+define('DB_PASS', 'Kusalmendis1!'); //Add your DB pass
+define('DB_NAME', 'api'); //Add your DB Name
+
+//APPROOT
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URLROOT (Dynamic links)
+define('URLROOT', 'http://localhost/api.flexguru.tk');
+
+//Sitename
+define('SITENAME', 'Flexguru API Services');
+
+//Secret key
+define('SECRET_KEY', 'Abc123');
+define('REFRESH_KEY', 'ADs121');
