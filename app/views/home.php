922bf57d34a98dbe4d1cde0bdda14a403f0a7dbb
kesaralive <kesaralive@gmail.com>
2022-01-02 06:11:25 +0530

    feat: create contact us section in homepage

--- app/views/home.php
+++ app/views/home.php
@@ -219,8 +219,24 @@
                     <i class="text-gray-600 fas fa-chevron-down"></i>
                 </div>
                 <button class="mt-12 flex self-center btn btn-purple hover:bg-gray-100 hover:text-gray-600" type="button">
-                   More Info
+                    More Info
                 </button>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Us -->
+    <section class="bg-indigo-500 text-white py-20">
+        <div class="container">
+            <div class="sm:w-3/4 lg:w-2/4 mx-auto">
+                <p class="font-light uppercase text-center mb-8 ">500+ ALREADY JOINED</p>
+                <h1 class="text-3xl text-center">Stay up-to-date with what we're doing</h1>
+                <div class="flex flex-col sm:flex-row gap-6 mt-8">
+                    <input type="text" placeholder="Enter your email address" name="email" id="email" class="focus:outline-none flex-1 px-2 py-3 rounded-md text-gray-600">
+                    <button class="btn bg-orange-600 hover:bg-gray-100 hover:text-gray-700" type="button">
+                        Contact Us
+                    </button>
+                </div>
             </div>
         </div>
     </section>
