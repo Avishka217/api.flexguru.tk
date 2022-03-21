afc9ba38f18118a7a51d56f1cf2ea6f0793225be
kesaralive@gmail.com <90370604+kesaralive@users.noreply.github.com>
2022-03-21 10:09:09 +0530

    feat: add medium & method to service gig

--- app/models/Service.php
+++ app/models/Service.php
@@ -11,7 +11,7 @@ class Service extends Model
 
     public function create($data)
     {
-        $this->db->query("INSERT INTO " . $this->table . "(tuid, title, lesson, description, price, image, duration,rating,subject,revisions) VALUES(:tuid, :title, :lesson, :description, :price, :image, :duration, :rating, :subject, :revisions)");
+        $this->db->query("INSERT INTO " . $this->table . "(tuid, title, lesson, description, price, image, duration, method, medium, rating, subject, revisions) VALUES(:tuid, :title, :lesson, :description, :price, :image, :duration, :method, :medium, :rating, :subject, :revisions)");
         $this->db->bind(':tuid', $data['tutorid']);
         $this->db->bind(':title', $data['title']);
         $this->db->bind(':lesson', $data['lesson']);
@@ -19,6 +19,8 @@ class Service extends Model
         $this->db->bind(':price', $data['price']);
         $this->db->bind(':revisions', $data['revisions']);
         $this->db->bind(':duration', $data['duration']);
+        $this->db->bind(':method', $data['method']);
+        $this->db->bind(':medium', $data['medium']);
         $this->db->bind(':subject', $data['subject']);
         $this->db->bind(':image', $data['image']);
         $this->db->bind(':rating', 0);
